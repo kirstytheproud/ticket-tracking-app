3d61fd47f27ffc4418c9c88012361129c70ef3f0
kirstytheproud <k.joproud@gmail.com>
2022-03-09 14:53:55 +0000

    counter implemented

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import { useState } from 'react';
 import "./Counter.scss"
 
-const Counter = () => {
+const Counter = ({}) => {
+  const [counter, setCounter] = useState(0);
+
+  const handleDecrement = () => {
+    setCounter(counter - 1);
+  };
+
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
   return (
-    <div className="tracker-card__counter">
-      <h2>Counter</h2>
-      + -
-      </div>
-  )
-}
+    <div className="counter-container">
+      <h3 className="counter-container__heading">Counter</h3>
+      <div className="counter-container__content">
+      <button onClick={handleDecrement}>-</button>
+      <p className="counter-container__counter">{counter}</p>
+      <button onClick={handleIncrement}>+</button>
+    </div>
+    </div>
+  );
+};
 
 export default Counter;
